95c54e0ba3c98e18c78e2602d8e1fdd524851a5f
amien kurniawan <offdevamienk@gmail.com>
2020-02-22 22:26:24 +0700

    update (add new package and change script.json)

--- frontend 14/src/js/About/About.js
+++ frontend 14/src/js/About/About.js
@@ -1,2 +1,5 @@
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import gambar from '../../img/about.jpg';
+
+var images = document.getElementById("images");
+images.src = gambar; 
--- frontend 14/webpack-production.config.js
+++ frontend 14/webpack-production.config.js
@@ -27,8 +27,26 @@ module.exports = {
                 }]
             },
             {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                test: /\.(sa|sc|c)ss$/,
+                use: [{
+                      loader: 'style-loader', // inject CSS to page
+                    }, {
+                      loader: 'css-loader', // translates CSS into CommonJS modules
+                    }, 
+                    // {
+                    //   loader: 'postcss-loader', // Run post css actions
+                    //   options: {
+                    //     plugins: function () { // post css plugins, can be exported to postcss.config.js
+                    //       return [
+                    //         require('precss'),
+                    //         require('autoprefixer')
+                    //       ];
+                    //     }
+                    //   }
+                    // },
+                     {
+                      loader: 'sass-loader' // compiles Sass to CSS
+                    }],
             },
             {
                 test: /\.js$/,
