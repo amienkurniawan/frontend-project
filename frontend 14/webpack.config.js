065618644d2e21ed8f7a68ddadb6d3d36a8a0bea
amien kurniawan <offdevamienk@gmail.com>
2020-02-22 20:05:50 +0700

    update (add new training frontend-14)

---
+++ frontend 14/webpack.config.js
@@ -0,0 +1,55 @@
+const _ = require('lodash');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+const PAGE = ['About', 'Experience', 'Contact'];
+const entry = _.map(PAGE, page => './src/js/' + page + '/' + page + '.js');
+
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: entry,
+    output: {
+        filename: 'bundle.js',
+        pathinfo: false,
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [{
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images',
+                        esModule: false,
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    ],
+}
